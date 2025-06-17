c6615802b1b069b14781ddfcc49aa7f5a1e1ff0a
AMAN7040 <amansingh79620@gmail.com>
2025-06-17 22:54:01 +0530

    feat(theme): add utility to detect system theme and apply class to html

---
+++ src/lib/theme.ts
@@ -0,0 +1,13 @@
+import { Theme } from "@/types/themeType";
+
+export const getSystemTheme = (): Theme => {
+  return window.matchMedia("(prefers-color-scheme : dark)").matches
+    ? "dark"
+    : "light";
+};
+
+export const applyTheme = (theme: Theme) => {
+  const root = document.documentElement;
+  root.classList.remove("dark", "light");
+  root.classList.add(theme);
+};
