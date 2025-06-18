4491ba4e6e03e7094aa43bf7f1abf86dd96cdfac
AMAN7040 <amansingh79620@gmail.com>
2025-06-18 23:59:47 +0530

    fix(layout): restructure chat layout with fixed header and scrollable chat window

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,26 @@
+import ChatWindow from "@/components/chat/ChatWindow";
 import AuthGuard from "@/components/layout/AuthGuard";
-import ChatLayout from "@/components/layout/ChatLayout";
+import ChatInput from "@/components/layout/ChatLayout";
+import ToggleThemeClient from "@/components/theme/ToggleThemeCient";
+
 import React from "react";
 
 const page = () => {
   return (
     <AuthGuard>
-      <ChatLayout />
+      <div className="min-h-screen flex flex-col bg-background text-foreground">
+        <header className="w-full px-4 py-2 flex justify-end bg-[var(--wa-light-bg)] dark:bg-[var(--wa-dark-bg)] border-b border-border">
+          <ToggleThemeClient />
+        </header>
+
+        <main className="flex-1 flex flex-col max-w-4xl w-full mx-auto">
+          <div className="flex-1 overflow-y-auto">
+            <ChatWindow />
+          </div>
+
+          <ChatInput />
+        </main>
+      </div>
     </AuthGuard>
   );
 };
